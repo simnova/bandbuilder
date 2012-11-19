33a2a4bcdff7b9ef76266ce23890b36c4ea7e25a
Patrick Gidich <patrick@simnova.com>
2012-11-19 10:57:53 -0500

    fixing issue in jsdefaults

--- assets/js/libs/jsdefaults.js
+++ assets/js/libs/jsdefaults.js
@@ -3,7 +3,7 @@ define([], function () {
 
   var production = "buildyourband.azurewebsites.net";
   var staging = "bandbuilder.soscale.com";
-  var local = "localhost":
+  var local = "localhost";
 
 
   // static properties
@@ -20,21 +20,21 @@ define([], function () {
 
     base staging:
       JsDefaults.facebook.appId = '416912428373385';
-      JsDefaults.facebook.fanpageUrl= 'http://www.facebook.com/pages/12378123hertz/188563874521119';
-      JsDefaults.facebook.fanpageId= 416912428373385;
+      JsDefaults.facebook.fanpageUrl = 'http://www.facebook.com/pages/12378123hertz/188563874521119';
+      JsDefaults.facebook.fanpageId = 416912428373385;
       break;
 
     case local:
       JsDefaults.facebook.appId = '416912428373385';
-      JsDefaults.facebook.fanpageUrl= 'http://www.facebook.com/pages/12378123hertz/188563874521119';
-      JsDefaults.facebook.fanpageId= 416912428373385;
+      JsDefaults.facebook.fanpageUrl = 'http://www.facebook.com/pages/12378123hertz/188563874521119';
+      JsDefaults.facebook.fanpageId = 416912428373385;
       break;
 
     case production:
     default:
       JsDefaults.facebook.appId = '552144748135209';
-      JsDefaults.facebook.fanpageUrl= 'http://www.facebook.com/hertz';
-      JsDefaults.facebook.fanpageId= 91062694654;
+      JsDefaults.facebook.fanpageUrl = 'http://www.facebook.com/hertz';
+      JsDefaults.facebook.fanpageId = 91062694654;
       break;
 
   }//switch
